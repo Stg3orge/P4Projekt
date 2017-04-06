8fb0ae116ca56e8ab586784defe23edd7cb2845d
Rasmus Thomsen <Rasmuskjettrup@gmail.com>
2017-04-06 10:57:46 +0200

    pleb

--- p4_interpreter_3/p4_interpreter_3/Nodes/DeclarationCommands.cs
+++ p4_interpreter_3/p4_interpreter_3/Nodes/DeclarationCommands.cs
@@ -11,8 +11,6 @@
             this.statement2 = statement2;
         }
 
-<<<<<<< HEAD
-
         public override string ToString()
         {
             string returnstring = "";
@@ -33,12 +31,8 @@
 
 
         }
-    }
-=======
-        public override string ToString()
-        {
-            return statement1.ToString() + " " + statement2.ToString() + " ";
-        }
+    
+
     }
 }
->>>>>>> 24310a8cf9f1ebce08c35a869f3b7aea9a472e61
+
