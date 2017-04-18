135a6d72c11bf1d0f9d6ad901b455fc07475741c
benjamin madsen <benjamin madsen>
2017-04-18 14:43:20 +0200

    .

--- p4_interpreter_visitor/p4_interpreter_01.Tests/UnitTest1.cs
+++ p4_interpreter_visitor/p4_interpreter_01.Tests/UnitTest1.cs
@@ -6,9 +6,28 @@ namespace p4_interpreter_01.Tests
     [TestClass]
     public class UnitTest1
     {
+        SymbolTable symboltable = new SymbolTable();
         [TestMethod]
+        public void OpenScopeTesting()
+        {
+            //Arrange 
+            int expected = 0;
+            TestMethod1();
+            object value = null;
+            
+
+            symboltable.AddToTable("Test", "test", null);
+
+
+            //Act
+            
+            //Assert
+            //Assert.AreEqual(expected, actual);
+        }
         public void TestMethod1()
         {
+            
+            symboltable.OpenScope();
         }
     }
 }
