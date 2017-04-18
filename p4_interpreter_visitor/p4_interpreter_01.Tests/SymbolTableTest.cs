14a95d60c68b83d78aaf9bec71c3744669dc6ea6
benjamin madsen <benjamin madsen>
2017-04-18 15:12:16 +0200

    Benjamin

--- p4_interpreter_visitor/p4_interpreter_01.Tests/SymbolTableTest.cs
+++ p4_interpreter_visitor/p4_interpreter_01.Tests/SymbolTableTest.cs
@@ -10,10 +10,30 @@ namespace p4_interpreter_01.Tests
     [TestClass]
     public class SymbolTableTest
     {
+        SymbolTable symboltable = new SymbolTable();
         [TestMethod]
+        public void OpenScopeTesting()
+        {
+            //Arrange 
+            symboltable.AddToTable("global1", "integer", 0);
+            symboltable.AddToTable("global2", "integer", 0);
+            symboltable.OpenScope();
+            symboltable.AddToTable("metode1", "integer", 0);
+            symboltable.OpenScope();
+            symboltable.AddToTable("metode2", "integer", 0);
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(false, symboltable.ContainsName("metode1"));
+            //assert at global1, global2, metode2 kan findes
+            //assert at global1, global2 har scope == 0
+            //assert at metode2 har scope == 2
+        }
         public void TestMethod1()
         {
 
+            symboltable.OpenScope();
         }
     }
 }
