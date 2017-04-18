1530524ae453e88de767b3ccf77f9a881accd964
mathias <mathias@DESKTOP-ADMS65S>
2017-04-18 14:59:57 +0200

    .

--- p4_interpreter_visitor/p4_interpreter_01.Tests/UnitTest1.cs
+++ p4_interpreter_visitor/p4_interpreter_01.Tests/UnitTest1.cs
@@ -11,18 +11,20 @@ namespace p4_interpreter_01.Tests
         public void OpenScopeTesting()
         {
             //Arrange 
-            int expected = 0;
-            TestMethod1();
-            object value = null;
+            symboltable.AddToTable("global1", "integer", 0);
+            symboltable.AddToTable("global2", "integer", 0);
+            symboltable.OpenScope();
+            symboltable.AddToTable("metode1", "integer", 0);
+            symboltable.OpenScope();
+            symboltable.AddToTable("metode2", "integer", 0);
             
-
-            symboltable.AddToTable("Test", "test", null);
-
-
             //Act
-            
+
             //Assert
-            //Assert.AreEqual(expected, actual);
+            Assert.AreEqual(false, symboltable.ContainsName("metode1"));
+            //assert at global1, global2, metode2 kan findes
+            //assert at global1, global2 har scope == 0
+            //assert at metode2 har scope == 2
         }
         public void TestMethod1()
         {
